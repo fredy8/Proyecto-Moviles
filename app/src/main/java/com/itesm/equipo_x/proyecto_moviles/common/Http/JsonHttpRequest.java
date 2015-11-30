e0839b81d4f4036b6cad528fd63cd1fc6770759e
fredy8 <fredy.altamirano8@gmail.com>
2015-11-29 19:42:42 -0600

    add support for querystring in api class

--- app/src/main/java/com/itesm/equipo_x/proyecto_moviles/common/Http/JsonHttpRequest.java
+++ app/src/main/java/com/itesm/equipo_x/proyecto_moviles/common/Http/JsonHttpRequest.java
@@ -14,6 +14,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
